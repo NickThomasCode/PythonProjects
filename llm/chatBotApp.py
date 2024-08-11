a07284bb5bd461f2162fc8f003bf4e5ca95de658
NickThomasCode <thomasnick46@gmail.com>
2024-08-11 16:14:27 -0400

    Nick: working on chat input

--- llm/chatBotApp.py
+++ llm/chatBotApp.py
@@ -6,9 +6,10 @@ st.title("ChatBot")
 
 llm = Ollama(model="mistral")
 
-import numpy as np
 
+message("Please enter your prompt") 
 prompt = st.chat_input("Enter your prompt:")
 if prompt:
+    message(prompt, is_user=True) 
     resp = llm.invoke(prompt)
-    st.write(resp)
+    message(resp) 
