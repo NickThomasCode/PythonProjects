e1f73fe92919b604039dc5503ab3104953d7247d
Nick <thomasnick46@gmail.com>
2025-02-17 16:30:07 -0500

    Nick: sentiment analysis example work.

---
+++ cfbPlayPredictor/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+# import sklearn
+import joblib
+
+#loaded_model = joblib.load('cfbplaypred.joblib')
+
+st.title('Michigan Football Play Predictor')
+
---
+++ cfbPlayPredictor/load_model.py
@@ -0,0 +1,6 @@
+import sklearn
+import joblib
+
+loaded_model = joblib.load('cfbplaypred.joblib')
+
+print('Done')
